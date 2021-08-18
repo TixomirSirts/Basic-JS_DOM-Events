3fbef8b062bbfd3ebd0e0af305ce85b90acdfeb5
TixomirSirts <tihomir.sirts@yandex.ru>
2021-08-18 22:53:37 +0300

    Upgraded Task-9.5 & Navigation & CSS

--- task4/script.js
+++ task4/script.js
@@ -1,8 +1,10 @@
 const link = document.querySelector('#link');
+link.addEventListener('click', linkTextChange);
 
-link.addEventListener('click', (event) => {
+function linkTextChange(event) {
     let linkText = link.textContent;
     linkText = prompt('Введите новый текст для ссылки');
     linkText === '' || linkText === null ? linkText = 'Измените текст ссылки' : link.textContent = linkText;
     event.preventDefault();
-})
+    link.removeEventListener('click', linkTextChange);
+}
