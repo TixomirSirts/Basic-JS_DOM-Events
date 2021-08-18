64596ad18d70e39fcee19b7976c849331a667534
TixomirSirts <tihomir.sirts@yandex.ru>
2021-08-18 21:39:47 +0300

    Added CSS

--- task4/script.js
+++ task4/script.js
@@ -4,6 +4,5 @@ link.addEventListener('click', (event) => {
     let linkText = link.textContent;
     linkText = prompt('Введите новый текст для ссылки');
     linkText === '' || linkText === null ? linkText = 'Измените текст ссылки' : link.textContent = linkText;
-    // link.textContent = linkText;
     event.preventDefault();
 })
